b4bab6974dfb2fab7d5bb426d8c5a34bc6c6948f
v1acheslav <vyacheslav@imena.ua>
2019-07-11 14:12:31 +0300

    full change

--- calc.py
+++ calc.py
@@ -1,48 +1,45 @@
 #!/usr/bin/python3
 
-
-class ProductPrice:
-    pass
-
-class product:
-
-    def __init__(self, price, name, description =""):
-        self.price = ProductPrice()
-        self.name = name
-        self.description = description
-
-    def get_sell_price (self, vat=0.2):
-        print(self.name + " product")
-        return self.price + self.price * vat
-
-
-tv = product(10, 'tv')
-radio = product(5, 'radio')
-
-print(tv.get_sell_price())
-print(radio.get_sell_price())
-
-
-class Product2(product):
-
-    def get_sell_price(self, vat=0.2):
-        print(self.name + " product2")
-        return self.price + 10
-
-
-
-tv2 = Product2(10, 'tv2')
-
-print(tv2.get_sell_price())
-
-
-
-
-
-
-
-
-
-
-
-
+print('Welcome to Calculator')
+def calculate():
+    expression = input('Enter an expression: ')
+    expression_result = check_input((expression).replace(' ', ''))
+    while not expression_result:
+        print('You did not enter a valid expression')
+        expression = input('Enter an expression: ')
+        expression_result = check_input((expression).replace(' ', ''))
+    
+    if expression_result:
+        print(eval(expression_result))
+    else:
+        print("Expression result is False")
+def check_input(input_expression):
+    
+    input_expression = str(input_expression)
+    # Check the basics
+    if len(input_expression) < 3:
+        print('len is less than 3')
+        return False
+    # Check if we are getting passed correct characters
+    for character in input_expression:
+        if character not in '0123456789/*+-':
+            print('{} not in given value'.format(character))
+            return False
+    # Things like /23 are not valid
+    if input_expression[0] in '/*+':
+        print('Are you sure you know what you\'re doing?')
+        return False
+    return input_expression
+calculate()
+while True:
+    calc_again = input('''
+        Do you want to calculate again?
+        Please type Y for YES or N for NO.
+        ''')
+    if calc_again.upper() == 'Y':
+        calculate()
+    elif calc_again.upper() == 'N':
+        print('See you later.')
+        break
+    else:
+        continue
